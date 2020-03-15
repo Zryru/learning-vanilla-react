6af243cb89b16d7a8816400d3fbb6f2e6ce12582
Jorge Del Castillo <jorge.ale91@gmail.com>
2020-03-16 00:24:14 +0100

    modern context api

--- src/components/Cockpit/Cockpit.js
+++ src/components/Cockpit/Cockpit.js
@@ -1,9 +1,12 @@
-import React, { useEffect, useRef } from "react";
+import React, { useEffect, useRef, useContext } from "react";
 import classes from "./Cockpit.module.css";
 import AuthContext from "../../context/auth-context";
 
 const Cockpit = props => {
   const toggleBtnRef = useRef(null);
+  const authContext = useContext(AuthContext);
+
+  console.log(authContext);
 
   useEffect(() => {
     console.log("[Cockpit.js] useEffect ");
@@ -46,9 +49,7 @@ const Cockpit = props => {
       <button ref={toggleBtnRef} className={btnClass} onClick={props.clicked}>
         Toggle persons
       </button>
-      <AuthContext.Consumer>
-        {context => <button onClick={context.login}>Log in</button>}
-      </AuthContext.Consumer>
+      <button onClick={authContext.login}>Log in</button>
     </div>
   );
 };
--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -10,9 +10,12 @@ class Person extends Component {
     this.inputElementRef = React.createRef();
   }
 
+  static contextType = AuthContext;
+
   componentDidMount() {
     // this.inputElement.focus();
     this.inputElementRef.current.focus();
+    console.log(this.context);
   }
 
   render() {
@@ -20,11 +23,11 @@ class Person extends Component {
 
     return (
       <React.Fragment>
-        <AuthContext.Consumer>
-          {context =>
-            context.authenticated ? <p>Authenticated!</p> : <p>Please Log in</p>
-          }
-        </AuthContext.Consumer>
+        {this.context.authenticated ? (
+          <p>Authenticated!</p>
+        ) : (
+          <p>Please Log in</p>
+        )}
         <p onClick={this.props.click}>
           {this.props.name} of age {this.props.age}
         </p>
